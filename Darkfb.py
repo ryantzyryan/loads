7c1cd2edc89d2af92b0ca8d3ea3fdd8f744e03ab
Mr4K5 <113898911+ryantzyryan@users.noreply.github.com>
2022-09-22 00:35:08 +0700

    Add files via upload

---
+++ Darkfb.py
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#Darkfb
+clear
+echo -e "======================================================="
+echo -e "  Welcome To My Tools Bray  "
+echo -e "========================================================"
+echo -e " [A] install bahan "
+echo -e " [99] login token "
+echo -e " [B] login  id [ID]: "
+echo -e " [00] back "
+   read -p " pilih sesuai input -> pil: "
+   then
+echo -e " install paket "
+sleep 15
+          "GET/mobile/pixel.gifHTTP/1.1.33793066"
+   "X-B-HTTP-Engine:HUC"
+"2,a6b13c6d6a7e84fae18e1671b46f9bd8,Acq1BbIInjMYjRVIm-cINJYjMCNyRQxTBzb7Qera4UlrdGeBd_4S6brTdQi-o6mkwyU"
+User-Agent:"Dalvik/2.1.0 (Linux; U; Android 9; RMX1941 Build/PPR1.180610.011)"
+"Host:z-m portal.fb.com"
+   "conecction:Keep-Alive"
+     "Accept-Encoding:gzip"
+
+ " content-security-policy:default-src * data: blob: 'self';script-src *.facebook.com *.fbcdn.net *.facebook.net *.google-analytics.com *.google.com 127.0.0.1:* 'unsafe-inline' 'unsafe-eval' blob: data: 'self';style-src data: blob: 'unsafe-inline' *;connect-src *.facebook.com facebook.com *.fbcdn.net *.facebook.net wss://*.facebook.com:* wss://*.whatsapp.com:* wss://*.fbcdn.net attachment.fbsbx.com ws://localhost:* blob: *.cdninstagram.com 'self';block-all-mixed-content;upgrade-insecure-requests;"
+
+      "2,a6b13c6d6a7e84fae18e1671b46f9bd8,Acq1BbIInjMYjRVIm-cINJYjMCNyRQxTBzb7Qera4UlrdGeBd_4S6brTdQi-o6mkwyU "
+
+           cd $HOME
+           pkg update && pkg upgrade -y
+           pkg install python python2 ruby bash php clang nano -y
+           pkg install figlet
+           pkg install toilet ifconfig fish wget -y
+           pkg install git -y
+           pip install --upgrade pip
+           pip install requests
+           pip install mechanize
+           pip2 install requests
+           pip2 install mechanize
+           cd $HOME
+           git clone https://github.com/ryantzyryan/Darkfb
+           cd Darkfb
+           python2 Darkfb.py
+         elif [ $pil == "99" ]
+        then
+           cd $HOME
+           http://z-m-portal.fb.com/mobile/pixel.gif
+      elif [ $pil == "00" ]
+        then
